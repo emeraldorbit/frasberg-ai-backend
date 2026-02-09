03e42b8f5c1e6834d9cda5aad9d744f9769e0eb6
EmeraldOrbit <contact@168emeraldestatesllc.com>
2026-02-09 18:33:48 +0000

    Release Sofia Core 6.5.0 - Performance & Polish

--- sdk/python/sofia_sdk/__init__.py
+++ sdk/python/sofia_sdk/__init__.py
@@ -3,5 +3,5 @@
 from .client import SofiaClient
 from .exceptions import SofiaException, APIError, ValidationError
 
-__version__ = "6.0.0"
+__version__ = "6.5.0"
 __all__ = ["SofiaClient", "SofiaException", "APIError", "ValidationError"]
